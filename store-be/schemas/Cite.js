cbd4f7268619eb4c31a4f059f4163cca9ed7a5cd
Carlito <rody.castillo.tacuri@gmail.com>
2022-03-09 17:34:59 -0500

    feat[schemas]: add new user and cite

---
+++ store-be/schemas/Cite.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const CiteSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true },
+    date: { type: Date, required: true },
+    description: { type: String, required: true },
+    cite: { type: Object, required: true },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Cite", CiteSchema);
